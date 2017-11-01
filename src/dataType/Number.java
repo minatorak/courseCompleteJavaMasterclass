e3492ac5ad7ead5c38e0bdea87637505f66f6843
sirirak <sirirak@localhost.localdomain>
2017-11-02 02:17:39 +0700

    Initial commit

---
+++ src/dataType/Number.java
@@ -0,0 +1,20 @@
+package dataType;
+
+public class Number {
+    public static void main(String[] args){
+        //int 32 bits
+        int intminValue = -2_147_483_648;
+        int twoval = 2000;
+        int intmaxValue = 2147483647;
+
+        //byte 8 bits
+        byte myByteVal = -128;
+
+        //16 bits
+        short intShortVal = 32767;
+
+        long LongVal = 9_223_372_036_854_775_807L;
+
+
+    }
+}
---
+++ src/helloworld/Hello.java
@@ -0,0 +1,13 @@
+package helloworld;
+
+public class Hello {
+    public static void main(String[] args){
+        System.out.println("");
+        System.out.println("Hello world" +
+                "feed");
+        int myFirstNumber = 10+5-2*10;
+        int mySeconNumber = 12;
+        String sum = "sum" + 10 + 5 * 10;
+
+    }
+}
